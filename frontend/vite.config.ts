a7de9c63a8d1a0a5dbfe9daca6bc9e9e2919c691
Iurii Matveev <yury.matveev.1983@gmail.com>
2025-11-19 08:16:35 +0300

    build node_modules to vendor.js, xlsx as separate module. So new builds will not make new big files in static folder.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    //visualizer({ open: true, gzipSize: true, brotliSize: true }),
+  ],
   build: {
     outDir: "../backend/static",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/xlsx/xlsx")) {
+            return "xlsx";
+          }
+          // All node_modules go into vendor chunk
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
   },
   server: {
     proxy: {
